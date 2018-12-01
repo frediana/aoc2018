9b5967df81b2d4a8bba764b7c7f9958cc7e3e442
Adrien Frediani <frediani.adrien@fairway.digital>
2018-12-01 21:59:00 +0100

    day1(part2): make the test pass

--- src/day1/day1.js
+++ src/day1/day1.js
@@ -1,6 +1,28 @@
 const part1 = (values) => values.reduce((acc, current) => acc + current, 0);
 
-const part2 = (values) => values[0];
+const part2 = (values) => {
+	const seen = new Set([0]);
+	let i = 0;
+	let sum = 0;
+
+	while (true) {
+		sum += values[i];
+
+		if (seen.has(sum)) {
+			break;
+		}
+
+		seen.add(sum);
+
+		if (i === values.length - 1) {
+			i = 0;
+		} else {
+			i += 1;
+		}
+	}
+
+	return sum;
+};
 
 export { part1 };
 export { part2 };
