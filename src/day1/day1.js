fc86b6dc6933ae4c823b5defdf93f5be605f5c3c
Adrien Frediani <frediani.adrien@fairway.digital>
2018-12-01 14:56:06 +0100

    day1(resolver): brute force resolver using class

--- src/day1/day1.js
+++ src/day1/day1.js
@@ -1,3 +1,21 @@
-export default {
-	resolve: () => true
-};
+class FrequencySolver {
+	constructor(initialValue) {
+		this.currentValue = initialValue || 0;
+	}
+
+	pushValue(value) {
+		this.currentValue += value;
+	}
+
+	pushValues(values) {
+		values.forEach((v) => this.pushValue(v));
+	}
+
+	get value() {
+		return this.currentValue;
+	}
+}
+
+export { FrequencySolver };
+
+export default {};
--- src/day1/day1.test.js
+++ src/day1/day1.test.js
@@ -1,7 +1,49 @@
-import day1 from './day1';
+import { FrequencySolver } from './day1';
 
 describe('day1', () => {
-	it('should be defined', () => {
-		expect(day1.resolve()).toBe(true);
+	let frequencySolver;
+
+	describe('Given a null initial frequency', () => {
+		beforeEach(() => {
+			frequencySolver = new FrequencySolver();
+		});
+
+		describe('pushValue', () => {
+			describe('When frequency change by a negative value', () => {
+				it('should decrease frequency', () => {
+					frequencySolver.pushValue(-1);
+					expect(frequencySolver.value).toEqual(-1);
+				});
+			});
+
+			describe('When frequency change by a positive value', () => {
+				it('sould increase frequency', () => {
+					frequencySolver.pushValue(1);
+					expect(frequencySolver.value).toEqual(1);
+				});
+			});
+
+			describe('When frequency change by 0', () => {
+				it('sould leave frequency un-changed', () => {
+					frequencySolver.pushValue(0);
+					expect(frequencySolver.value).toEqual(0);
+				});
+			});
+		});
+
+		describe('pushValues', () => {
+			describe.each([[1, 1, 1, 3], [1, 1, -2, 0], [-1, -2, -3, -6]])(
+				'Given (%i, %i, %i)',
+				(v1, v2, v3, expectedResult) => {
+					test(`Result should be ${expectedResult}`, () => {
+						frequencySolver = new FrequencySolver();
+						frequencySolver.pushValue(v1);
+						frequencySolver.pushValue(v2);
+						frequencySolver.pushValue(v3);
+						expect(frequencySolver.value).toEqual(expectedResult);
+					});
+				}
+			);
+		});
 	});
 });
