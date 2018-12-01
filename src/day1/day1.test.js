a57c812f9f9482059fb8965f54cca1438d15a7d1
Adrien Frediani <frediani.adrien@fairway.digital>
2018-12-01 17:57:52 +0100

    day1(tests): reword ut

--- src/day1/day1.test.js
+++ src/day1/day1.test.js
@@ -2,7 +2,7 @@ import frequencySolver from './day1';
 
 describe('day1', () => {
 	describe.each([[[1, 1, 1], 3], [[1, 1, -2], 0], [[-1, -2, -3], -6]])(
-		'Given (%i, %i, %i)',
+		'Given %p)',
 		(values, expectedResult) => {
 			test(`Result should be ${expectedResult}`, () => {
 				expect(frequencySolver(values)).toEqual(expectedResult);
