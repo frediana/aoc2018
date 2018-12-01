47daf68b71760a97396b36217ab9fb230b172b2b
Adrien Frediani <frediani.adrien@fairway.digital>
2018-12-01 17:05:54 +0100

    day1(resolver): simpler solution using reduce

--- src/day1/day1.js
+++ src/day1/day1.js
@@ -1,21 +1,21 @@
-class FrequencySolver {
-	constructor(initialValue) {
-		this.currentValue = initialValue || 0;
-	}
+// class FrequencySolver {
+// 	constructor(initialValue) {
+// 		this.currentValue = initialValue || 0;
+// 	}
+//
+// 	pushValue(value) {
+// 		this.currentValue += value;
+// 	}
+//
+// 	pushValues(values) {
+// 		values.forEach((v) => this.pushValue(v));
+// 	}
+//
+// 	get value() {
+// 		return this.currentValue;
+// 	}
+// }
 
-	pushValue(value) {
-		this.currentValue += value;
-	}
-
-	pushValues(values) {
-		values.forEach((v) => this.pushValue(v));
-	}
-
-	get value() {
-		return this.currentValue;
-	}
+export default function(values) {
+	return values.reduce((acc, current) => acc + current, 0);
 }
-
-export { FrequencySolver };
-
-export default {};
--- src/day1/day1.test.js
+++ src/day1/day1.test.js
@@ -1,49 +1,12 @@
-import { FrequencySolver } from './day1';
+import frequencySolver from './day1';
 
 describe('day1', () => {
-	let frequencySolver;
-
-	describe('Given a null initial frequency', () => {
-		beforeEach(() => {
-			frequencySolver = new FrequencySolver();
-		});
-
-		describe('pushValue', () => {
-			describe('When frequency change by a negative value', () => {
-				it('should decrease frequency', () => {
-					frequencySolver.pushValue(-1);
-					expect(frequencySolver.value).toEqual(-1);
-				});
+	describe.each([[[1, 1, 1], 3], [[1, 1, -2], 0], [[-1, -2, -3], -6]])(
+		'Given (%i, %i, %i)',
+		(values, expectedResult) => {
+			test(`Result should be ${expectedResult}`, () => {
+				expect(frequencySolver(values)).toEqual(expectedResult);
 			});
-
-			describe('When frequency change by a positive value', () => {
-				it('sould increase frequency', () => {
-					frequencySolver.pushValue(1);
-					expect(frequencySolver.value).toEqual(1);
-				});
-			});
-
-			describe('When frequency change by 0', () => {
-				it('sould leave frequency un-changed', () => {
-					frequencySolver.pushValue(0);
-					expect(frequencySolver.value).toEqual(0);
-				});
-			});
-		});
-
-		describe('pushValues', () => {
-			describe.each([[1, 1, 1, 3], [1, 1, -2, 0], [-1, -2, -3, -6]])(
-				'Given (%i, %i, %i)',
-				(v1, v2, v3, expectedResult) => {
-					test(`Result should be ${expectedResult}`, () => {
-						frequencySolver = new FrequencySolver();
-						frequencySolver.pushValue(v1);
-						frequencySolver.pushValue(v2);
-						frequencySolver.pushValue(v3);
-						expect(frequencySolver.value).toEqual(expectedResult);
-					});
-				}
-			);
-		});
-	});
+		}
+	);
 });
