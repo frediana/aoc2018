aa4eafbb240a6b0ab53055bab8660627511224b9
Adrien Frediani <frediani.adrien@fairway.digital>
2018-12-01 18:49:57 +0100

    refactor(day1): tests

--- src/day1/day1.test.js
+++ src/day1/day1.test.js
@@ -2,7 +2,7 @@ import fs from 'fs';
 import readline from 'readline';
 import { part1 as frequencySolver } from './day1';
 
-describe('day1', () => {
+describe('day1 part 1', () => {
 	describe.each([[[1, 1, 1], 3], [[1, 1, -2], 0], [[-1, -2, -3], -6]])(
 		'Given %p)',
 		(values, expectedResult) => {
@@ -11,19 +11,19 @@ describe('day1', () => {
 			});
 		}
 	);
-});
 
-const myInterface = readline.createInterface({
-	input: fs.createReadStream(`${__dirname}/input.txt`)
-});
+	const myInterface = readline.createInterface({
+		input: fs.createReadStream(`${__dirname}/input.txt`)
+	});
 
-const data = [];
+	const data = [];
 
-myInterface.on('line', (line) => {
-	const value = parseInt(line, 10);
-	data.push(value);
-});
+	myInterface.on('line', (line) => {
+		const value = parseInt(line, 10);
+		data.push(value);
+	});
 
-myInterface.on('close', () => {
-	console.log(`Solution for part1 ====> ${frequencySolver(data)}`);
+	myInterface.on('close', () => {
+		console.log(`Solution for part1 ====> ${frequencySolver(data)}`);
+	});
 });
