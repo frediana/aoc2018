89768233fae28c14b3f128c3777793fdc27a40e1
Adrien Frediani <frediani.adrien@fairway.digital>
2018-12-01 17:06:26 +0100

    day1(resolver): remove dead code

--- src/day1/day1.js
+++ src/day1/day1.js
@@ -1,21 +1,3 @@
-// class FrequencySolver {
-// 	constructor(initialValue) {
-// 		this.currentValue = initialValue || 0;
-// 	}
-//
-// 	pushValue(value) {
-// 		this.currentValue += value;
-// 	}
-//
-// 	pushValues(values) {
-// 		values.forEach((v) => this.pushValue(v));
-// 	}
-//
-// 	get value() {
-// 		return this.currentValue;
-// 	}
-// }
-
 export default function(values) {
 	return values.reduce((acc, current) => acc + current, 0);
 }
