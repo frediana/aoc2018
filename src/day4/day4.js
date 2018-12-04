888458d0c460dd741dfb6dfd9f15e7d4aa216752
Adrien Frediani <frediani.adrien@fairway.digital>
2018-12-05 00:03:50 +0100

    brute stuff, not finished yet...

--- src/day4/day4.js
+++ src/day4/day4.js
@@ -1,3 +1,75 @@
-export default function() {
-  return 'day4';
-}
+const parseDateTime = (rawDate, rawTime) => {
+	return {
+		date: rawDate.substring(1),
+		time: rawTime.substring(0, rawTime.length - 1)
+	};
+};
+
+const parseAction = (rawAction) => {
+	const [type, rowId] = rawAction;
+	switch (type) {
+		case 'Guard':
+			return {
+				type: type.toLowerCase(),
+				id: rowId.substring(1)
+			};
+
+		case 'wakes':
+		case 'falls':
+			return { type };
+	}
+};
+
+// [time] action
+const parseShift = (shift) => {
+	const [rawDate, rawTime, ...rawActions] = shift.split(' ');
+	const { date, time } = parseDateTime(rawDate, rawTime);
+	const action = parseAction(rawActions);
+	return {
+		date,
+		time,
+		action
+	};
+};
+
+export const parseShifts = (shifts) => shifts.map((shift) => parseShift(shift));
+
+// return: [ guard: { id, shifts: [{ start, length }] ]
+export const getAllGuards = (shifts) => {
+	const parsedShifts = parseShifts(shifts);
+
+	const guards = parsedShifts.map((shift) => {
+		const guard = {};
+		if (shift.action.type === 'guard') {
+			guard.id = shift.action.id;
+		}
+
+		return guard;
+	});
+
+	return guards;
+};
+
+const getTotalTimeAsleep = (guard) =>
+	guard.shifts.reduce((totalTimeAsleep, shift) => shift.length, 0);
+
+// return guard: { id, shifts: [{ start, length }]
+export const getGuardMostTotalSleep = (shifts) => {
+	let guardMostAsleep;
+	let maxTimeAsleep = 0;
+	getAllGuards(shifts).forEach((guard) => {
+		const guardTotalTimeAsleep = getTotalTimeAsleep(guard.shifts);
+
+		if (guardTotalTimeAsleep > maxTimeAsleep) {
+			maxTimeAsleep = guardTotalTimeAsleep;
+			guardMostAsleep = guard.id;
+		}
+	});
+
+	return guardMostAsleep;
+};
+
+// return int [0..59]
+// const getMinuteMostAsleep = (guard) => {};
+
+export default {};
--- src/day4/day4.test.js
+++ src/day4/day4.test.js
@@ -1,7 +1,19 @@
-import day4 from './day4';
+import { parseShifts, getAllGuards } from './day4';
 
 describe('day4', () => {
 	test('should be defined', () => {
-		expect(day4()).toEqual('day4');
+		expect(parseShifts).toBeDefined();
 	});
 });
+
+const data = [
+	'[1518-11-01 00:00] Guard #10 begins shift',
+	'[1518-11-01 00:05] falls asleep',
+	'[1518-11-01 00:25] wakes up',
+	'[1518-11-01 00:30] falls asleep',
+	'[1518-11-01 00:55] wakes up',
+	'[1518-11-01 00:00] Guard #11 begins shift'
+];
+
+// console.log(parseShifts(data));
+console.log(getAllGuards(data));
